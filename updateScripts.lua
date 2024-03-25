c637a80a17b47fd1115db3bc5e998a8808a930a1
Khaleel <khaleelanjum0@gmail.com>
2024-03-26 04:34:22 +1100

    autoUpdate implemented

--- updateScripts.lua
+++ updateScripts.lua
@@ -1,1 +1,28 @@
-local scriptToUpdate = ""
+local function getScriptName(name)
+	local urlArray = {}
+	for word in string.gmatch(name, "([^*/]+)") do
+		table.insert(urlArray, word)
+	end
+	return urlArray[#urlArray]
+end
+
+local function updateScript()
+	local scriptToUpdate = "https://raw.githubusercontent.com/Khaleel432/CCScripts/main/runFarmer.lua"
+	local request = http.get(scriptToUpdate);
+	local scriptName = getScriptName(scriptToUpdate);
+	local isFileExists = fs.exists(scriptName)
+	print(scriptName)
+	if(isFileExists == false) then
+		local file = fs.open(scriptName, "w")
+		file.write(request.readAll())
+		file.close()
+	end
+	print(fs.exists(scriptName))
+	request.close()
+end
+
+local function start()
+	updateScript()
+end
+
+start()
