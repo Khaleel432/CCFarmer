3ebcd765e2cbe412b6916340e039873c67608f64
Khaleel <khaleelanjum0@gmail.com>
2024-03-26 18:52:51 +1100

    check fuel reserves

--- runFarmer.lua
+++ runFarmer.lua
@@ -1,11 +1,16 @@
 local function startFarming()
 	local sizeOfFarm = "9" -- Number of blocks from one diagonal edge to the other
-	local startingDirection = "East"
+	local minFuelLevel = 80
 	print("Initiating farming procedure")
 	print("Checking fuel")
 	local fuelLevel = turtle.getFuelLevel()
 	print(fuelLevel)
 	print(turtle.getFuelLimit())
+	if (fuelLevel < minFuelLevel) then
+		print("Insufficient fuel, please fuel turtle up")
+		return
+	end
+	
 end
 
 local function start()
